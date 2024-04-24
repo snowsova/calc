4240565977f18ae932a1328ef6f79f3a3b45d7f2
Bogdan <snowsova@tuta.io>
2024-04-24 13:13:36 +0200

    Calc: version 0.1

---
+++ README.md
@@ -0,0 +1,19 @@
+# Calc - Simple Calculator
+
+Calc is simple and minimal calculator for UNIX systems written in C
+
+## Installation
+
+```bash
+$ git clone https://github.com/snowsova/calc
+$ cd calc
+$ make
+$ sudo make install
+```
+
+After installing, you can run `man calc` for additional information
+
+## Contributing
+
+Of course i would be glad for yours pull requests!
+
--- calc.c
+++ calc.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double  calculate(double, char, double);
+void    usage();
+
+double
+calculate(double first_num, char operator, double second_num)
+{
+        switch (operator) {
+                case '+': return first_num + second_num;
+                case '-': return first_num - second_num;
+                case '*': /* FALLTHROUGH */
+                case 'x': return first_num * second_num;
+                case '%': return (int)first_num % (int)second_num;
+
+                case '/': return second_num == 0 ? 0 : first_num / second_num;
+                default:  usage();
+        }
+        return 0;
+}
+
+void
+usage()
+{
+        fprintf(stderr, "usage: calc <first_num> [+ - * or x %% /] <second_num>\n");
+        exit(EXIT_FAILURE);
+}
+
 int
 main(int argc, char *argv[])
 {
-        printf("%s\n", "Hello World");
+        if (argc < 2 || argc % 2 != 0)
+                usage();
+        
+        if (argc < 4) {
+                printf("%d\n", atoi(argv[1]));
+                return EXIT_SUCCESS;
+        }
+        
+        double result = calculate(atof(argv[1]), *argv[2], atof(argv[3]));
+        if (result == (int)result)
+                printf("%.0f\n", result);
+        else
+                printf("%.2f\n", result);
+
         return EXIT_SUCCESS;
 }
 
